92178c41e14e93c4bc24bc187d021d36351e80c7
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-06-16 20:41:29 -0300

    config base backend and star a mobile interface

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,8 +6,38 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Foi'})
+app.get('/:id', (req, res) => {
+  const { id } = req.params
+
+  const guitarStrings = {
+    e: '1',
+    B: '2',
+    G: '3',
+    D: '4',
+    A: '5',
+    E: '6',
+  }
+
+  switch (id) {
+    case guitarStrings.B:
+      res.json({ message: 'Corda B' });
+      break;
+    case guitarStrings.G:
+      res.json({ message: 'Corda G' });
+      break;
+    case guitarStrings.D:
+      res.json({ message: 'Corda D' });
+      break;
+    case guitarStrings.A:
+      res.json({ message: 'Corda A' });
+      break;
+    case guitarStrings.E:
+      res.json({ message: 'Corda E' });
+      break;
+    default:
+      res.json({ message: 'Corda e' });
+  }
+   
 })
 
 app.listen(3000);
